fa19ee34b223dcb531eb0692e659d66f067e0217
Decameron <czorile@gmail.com>
2021-01-23 00:04:39 +0200

    add pug and sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,53 @@
-const { src, dest } = require('gulp');
+const { src, dest, parallel, series } = require('gulp');
 const gulp = require('gulp');
-const pug = require('pug');
+const pug = require('gulp-pug');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const del = require('del');
+const sass = require('gulp-sass');
 
 const paths = {
   app: {
-    pug: './app/*',
-    scss: './app/scss/*',
+    pug: './app/pug/*',
+    sass: './app/sass/*',
     js: './app/js/*',
-    img: './app/img/*'
+    img: './app/img/*',
+    fonts: './app/fnt/*'
   },
   dist: {
-    html: './dist/hmtl/',
+    html: './dist/html/',
     css: './dist/css/',
-    js: './dist/js',
-    img: './dist/img'
+    js: './dist/js/',
+    img: './dist/img/'
   }
 };
 
 
-function doHtml(done) {
+function doHtml() {
   return src(paths.app.pug)
     .pipe(pug())
-    .pipe(dest(paths.app.html))
-    .pipe(browserSync.stream()),
-    done();
+    .pipe(dest(paths.dist.html));
 }
 
-function doCss(done) {
-  // tbu
+function doCss() {
+  return src(paths.app.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.dist.css))
+    .pipe(browserSync.stream());
 }
 
-exports.doHtmlTask = doHtml;
+function clean() {
+  return del('dist');
+}
+
+// just testing out
+const test = parallel(doHtml, doCss);
+const build = series(test, clean);
+
+exports.doHtml = doHtml;
+exports.doCss = doCss;
+exports.clean = clean;
+
+exports.build = build;
