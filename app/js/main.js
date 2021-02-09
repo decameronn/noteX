3ed68f58d884e2c5739f55469b31f7455399cac8
Constantin Zorile <czorile@gmail.com>
2021-02-09 23:15:13 +0200

    simplified boilerplate code

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,21 @@
-let notes = [];
-const note = document.querySelector('.nl-note');
-const input = document.addEventListener('click', createNote());
+// let notes = [];
+// const note = document.querySelector('.nl-note');
+// let input = document.addEventListener('click', createNote());
 
 
-function renderNotes() {
-  note.innerHTML = notes.map(createNote).join('');
-}
+// function renderNotes() {
+//   note.innerHTML = notes.map(createNote).join('');
+// }
 
-function createNote(note) {
-  const { title, text } = note;
+// function createNote(note) {
+//   const { title, text } = note;
 
-  return `
-    <div class="note">
-      <div class="note-title">${title}</div>
-      <div class="note-text">${text}</div>
-    </div>
-  `;
-}
+//   return `
+//     <div class="note">
+//       <div class="note-title">${title}</div>
+//       <div class="note-text">${text}</div>
+//     </div>
+//   `;
+// }
+
+// import './app/sass/style.sass';
