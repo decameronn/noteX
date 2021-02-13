7d70df4265c234394d68d81f781fd55d8c817973
Constantin Zorile <czorile@gmail.com>
2021-02-13 22:48:05 +0200

    simplify the design

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const days = ['mon', 'tues', 'wed'];
+
+function findItemInArray() {
+  let day = days.find(x => x === 'tues');
+  return day ? true : false;
+}
+
+// for (const i of days) { console.log(i); }
