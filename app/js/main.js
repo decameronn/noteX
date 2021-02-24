cea46919170954728132a0474a82081ce08630c4
Constantin Zorile <czorile@gmail.com>
2021-02-24 22:59:04 +0200

    remove redundant comment lines

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,6 @@
 
-let notesContainer = document.getElementById("notes-container");
-
 function createNewNote() {
   let div = document.createElement("div");
   div.classList.add("note");
-  // document.getElementById("notes-container").appendChild(div);
-  notesContainer.appendChild(div);
+  document.getElementById("notes-container").appendChild(div);
 }
-
-// TODO:
-/**
- * - addNote function
- * - createNote function with note content
- * - save new note in browser Storage 
- * ...
- * - connect dropbox account
- * - sync notes
- */
-
- 
