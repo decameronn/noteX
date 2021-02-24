34117fc9bb51e35dc6365970fa1700d1267150b2
Constantin Zorile <czorile@gmail.com>
2021-02-24 23:25:20 +0200

    work on insertNote function

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,29 @@
 
-let notesContainer = document.getElementById("notes-container");
-
-function createNewNote() {
-  let div = document.createElement("div");
-  div.classList.add("note");
-  // document.getElementById("notes-container").appendChild(div);
-  notesContainer.appendChild(div);
-}
-
 // TODO:
 /**
  * - addNote function
  * - createNote function with note content
  * - save new note in browser Storage 
  * ...
+ * ... after a long time
  * - connect dropbox account
  * - sync notes
  */
 
  
+let notesContainer = document.getElementById("notes-container");
+
+function insertNewNote() {
+  let note = document.createElement("div");  
+  note.classList.add("note");
+
+  let noteTitle = document.createTextNode("h3");
+  noteTitle.classList.add("note-title");
+  let noteText = document.createElement("textarea");
+  noteText.classList.add("note-body");
+
+  note.appendChild(noteText);
+  note.appendChild(noteTitle);
+
+  notesContainer.appendChild(div);
+}
