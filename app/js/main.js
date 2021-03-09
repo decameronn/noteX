f2427207b532ad50f3157a4c102ac25f0df03e2c
Constantin Zorile <czorile@gmail.com>
2021-03-09 22:50:34 +0200

    update js

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,28 @@
-console.log("pwa connected!");
+
+let notebookList = [];
+let notesList = [];
+let createNotebookButton = document.getElementById("create-notebook");
+
+
+function createNotebookItem() {
+  let div = document.createElement("div");
+
+}
+
+function addNotebook() {
+  notebookList.forEach(element => {
+    // createNotebookItem() on button click
+  });
+}
+
+function delNotebook() {
+
+}
+
+function addNote() {
+
+}
+
+function delNote() {
+
+}
