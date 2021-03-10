8555c98e1e76056d9d830dca9672738f6bc30e7d
Constantin Zorile <czorile@gmail.com>
2021-03-10 21:14:50 +0200

    add basic pseudocode for notebook list functionality

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,16 @@ let notebookList = [];
 let notesList = [];
 let createNotebookButton = document.getElementById("create-notebook");
 
+const NOTEBOOK_MAX_NR = 255
+
 
 function createNotebookItem() {
   let div = document.createElement("div");
+  
+  while number of notebooks >= 0
+    when clicking the create button
+      add a new item on the list (array push)
+        (this does not automatically add an empty note to the notebook)
 
 }
 
@@ -16,7 +23,10 @@ function addNotebook() {
 }
 
 function delNotebook() {
-
+  while the number of notebooks > 0
+    remove notebook item from list (array pop)
+    if the number of notebook == 0
+      grey out the del button
 }
 
 function addNote() {
