799165ba4bf52d3d64a5865b3dae70682a9f1bba
Constantin Zorile <czorile@gmail.com>
2021-03-21 21:24:17 +0200

    small refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -3,30 +3,31 @@ let notebookList = [];
 let notesList = [];
 let createNotebookButton = document.getElementById("create-notebook");
 
-const NOTEBOOK_MAX_NR = 255
-
-
 function createNotebookItem() {
-  let div = document.createElement("div");
-  
-  while number of notebooks >= 0
-    when clicking the create button
-      add a new item on the list (array push)
-        (this does not automatically add an empty note to the notebook)
-
+  let form = document.createElement("form");
 }
 
 function addNotebook() {
   notebookList.forEach(element => {
     // createNotebookItem() on button click
   });
+
+  /*
+  while number of notebooks >= 0
+  when clicking the create button
+    add a new item on the list (array push)
+    save the creation date:time for that note
+      (this does not automatically add an empty note to the notebook)
+  */
 }
 
 function delNotebook() {
+  /*
   while the number of notebooks > 0
     remove notebook item from list (array pop)
     if the number of notebook == 0
       grey out the del button
+  */
 }
 
 function addNote() {
